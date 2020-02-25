dc9956d87b95a4cfa7acc109638910ab260f0211
mantis78 <boon.tek@gmail.com>
2020-02-25 23:17:11 +0800

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate chrono;
+extern crate regex;
+
+use chrono::{DateTime, Local};
+use regex::Captures;
+use regex::Regex;
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let re = Regex::new(r"([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))").unwrap();
+    for line in io::stdin().lock().lines() {
+        let str = &line.unwrap();
+        let result = re.replace(str, |c: &Captures| {
+                let d = DateTime::parse_from_rfc3339(&c[0]).unwrap();
+                d.with_timezone(&Local).to_rfc3339()
+        });
+        
+        println!("{}", result);
+    }
+}
