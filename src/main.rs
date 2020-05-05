ffb820fb2acec9782058dcbd46f3e738803132bd
mantis78 <boon.tek@gmail.com>
2020-05-05 11:05:14 +0800

    Add spacing.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::io::prelude::*;
 
 fn main() {
     let re = Regex::new(r"([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))").unwrap();
+
     for line in io::stdin().lock().lines() {
         let str = &line.unwrap();
         let result = re.replace(str, |c: &Captures| {
